20e75d71f3ddba0518169efadc32c8d877467328
Unknown <skrzynska.konstancja@gmail.com>
2020-11-18 13:35:58 +0100

    new positioning function

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,29 @@
 function elementsPosition (){
-	const x = window.innerWidth;
-	const y = document.getElementById('home').offsetHeight;
-	const bottom = document.getElementsByClassName('home__header')[0].offsetHeight;
-	const hMenu = document.getElementById('menu_1').offsetHeight;
-	const wMenu = document.getElementById('menu_1').offsetWidth;
-	document.getElementById('menu_1').setAttribute('style','top:'+parseInt((y-2*hMenu)/6)+'px; left:'+parseInt((x-2*wMenu)/3.5)+'px');
-	document.getElementById('menu_2').setAttribute('style','bottom:'+parseInt((y-2*hMenu+bottom)/2)+'px; left:'+parseInt((x-2*wMenu)/3.5)+'px');
-	document.getElementById('menu_3').setAttribute('style','top:'+parseInt((y-2*hMenu)/6)+'px; right:'+parseInt((x-2*wMenu)/3.5)+'px');
-	document.getElementById('menu_4').setAttribute('style','bottom:'+parseInt((y-2*hMenu+bottom)/2)+'px; right:'+parseInt((x-2*wMenu)/3.5)+'px');
+	const menuDiamond = document.getElementById('menu_1');
+	const homeDiamond = document.getElementById('img-diamond');
+	const data={
+		top: homeDiamond.offsetTop,
+		left: homeDiamond.offsetLeft,
+		height: homeDiamond.offsetHeight,
+		hMenu: menuDiamond.offsetHeight,
+		wMenu: menuDiamond.offsetWidth
+	};
+	const coordinates={
+		x: parseInt(data.left - data.wMenu),
+		y1: parseInt(data.top - data.hMenu/2),
+		y2: parseInt(data.top - data.hMenu/2 + data.height)
+	};
+	coordinates.y1 < 0 ? 1 : coordinates.y1;
+	menuDiamond.setAttribute('style', `top:${coordinates.y1}px; left:${coordinates.x}px;`);
+	document.getElementById('menu_2').setAttribute('style', `top:${coordinates.y2}px; left:${coordinates.x}px;`);
+	document.getElementById('menu_3').setAttribute('style', `top:${coordinates.y1}px; right:${coordinates.x}px;`);
+	document.getElementById('menu_4').setAttribute('style', `top:${coordinates.y2}px; right:${coordinates.x}px;`);
 }
 window.onresize = elementsPosition;
 elementsPosition();
 
 let slideIndex = 0;
-showSlides();
 var slideTime;
-function currentSlide(n) {
-  slideIndex = n;
-  clearTimeout(slideTime);
-  showSlides();
-}
 function showSlides() {
     let i;
     const slides = document.getElementsByClassName("description");
@@ -36,11 +40,16 @@ function showSlides() {
     }
     dots[slideIndex-1].classList.add('active');
 }
-// MENU FUNCTIONS
+showSlides();
+function currentSlide(n) {
+	slideIndex = n;
+	clearTimeout(slideTime);
+	showSlides();
+}
+
 const menuButton = document.getElementById('menu-button');
 const menuList = document.getElementById('menu');
 const menuItems = menuList.children;
-//menu visible <->invisible
 menuButton.addEventListener('click',function(){
 	if(menuList.style.width != '100%'){
 		this.classList.add('menu-button_open');
@@ -50,7 +59,6 @@ menuButton.addEventListener('click',function(){
 		menuList.style.width = '0';
 	}
 } );
-//hide menu after click on link
 const hideAfter = function(){
 	menuButton.classList.remove('menu-button_open');
 	menuList.style.width = '0';
